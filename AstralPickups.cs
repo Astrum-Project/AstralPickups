0f644a9a4f24fbff3e7aa12d0e33b0a0655d3e5c
xKiraiChan <xKiraiChan@gmail.com>
2021-11-05 18:27:14 -0400

    add WallReach/GhostHand

--- AstralPickups.cs
+++ AstralPickups.cs
@@ -1,13 +1,15 @@
 ﻿using HarmonyLib;
 using MelonLoader;
 using System;
+using System.Linq;
 using System.Reflection;
 using VRC.SDKBase;
 using VRC.Udon.Wrapper.Modules;
 
-[assembly: MelonInfo(typeof(Astrum.AstralPickups), "AstralPickups", "0.2.1", downloadLink: "github.com/Astrum-Project/AstralPickups")]
+[assembly: MelonInfo(typeof(Astrum.AstralPickups), "AstralPickups", "0.3.0", downloadLink: "github.com/Astrum-Project/AstralPickups")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
+[assembly: MelonOptionalDependencies("AstralCore")]
 
 namespace Astrum
 {
@@ -26,6 +28,13 @@ namespace Astrum
             HarmonyInstance.Patch(typeof(ExternVRCSDK3ComponentsVRCPickup).GetMethod(nameof(ExternVRCSDK3ComponentsVRCPickup.__set_pickupable__SystemBoolean)), hkNoOp);
             HarmonyInstance.Patch(typeof(ExternVRCSDK3ComponentsVRCPickup).GetMethod(nameof(ExternVRCSDK3ComponentsVRCPickup.__set_allowManipulationWhenEquipped__SystemBoolean)), hkNoOp);
             HarmonyInstance.Patch(typeof(ExternVRCSDK3ComponentsVRCPickup).GetMethod(nameof(ExternVRCSDK3ComponentsVRCPickup.__set_proximity__SystemSingle)), hkNoOp);
+
+            if (AppDomain.CurrentDomain.GetAssemblies().Any(f => f.GetName().Name == "AstralCore"))
+            {
+                if (UnityEngine.Application.version.Contains("1134"))
+                    External.RemoveWallCheck(External.FindWallCheck());
+            }
+            else MelonLogger.Warning("AstralCore is missing, running at reduced functionality");
         }
 
         private static bool HookNoOp() => false;
@@ -35,6 +44,27 @@ namespace Astrum
             __instance.pickupable = true;
             __instance.allowManipulationWhenEquipped = true;
             __instance.proximity = float.MaxValue;
+        }
+
+        private static class External
+        {
+            public static IntPtr FindWallCheck()
+            {
+                return AstralCore.Utils.PatternScanner.Scan(
+                    "GameAssembly.dll", 
+                    "0F 85 C9 00 00 00" + //    jne GameAssembly.dll + AE40E0
+                    "48 8B 0D ????????" + //    mov rcx, [GameAssembly.dll + 7200EC0]
+                    "F6 81 2F 01 00 00 02" + // test byte ptr [rcx+12F], 2
+                    "74 0E" + //                je GameAssembly.dll + AE4035
+                    "44 39 B1 E0 00 00 00" + // cmp [rcx + E0],r14d
+                    "75 05" + //                jne GameAssembly.dll + AE4035??
+                    "E8 ????????" + //          call GameAssembly.il2cpp_runtime_class_init
+                    "33 D2" //                  xor edx, edx
+                );
+            }
+
+            public static void RemoveWallCheck(IntPtr address) => AstralCore.Utils.MemoryUtils.WriteBytes(address, new byte[6] { 0xE9, 0xCA, 0x00, 0x00, 0x00, 0x90 });
+            public static void RepairWallCheck(IntPtr address) => AstralCore.Utils.MemoryUtils.WriteBytes(address, new byte[6] { 0x0F, 0x85, 0xC9, 0x00, 0x00, 0x00 });
         }
     }
 }
