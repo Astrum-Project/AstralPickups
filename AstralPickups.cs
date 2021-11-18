cf5db43d38d8884c698a5f1bb761bedc3bfdde7e
xKiraiChan <xKiraiChan@gmail.com>
2021-11-18 01:01:35 -0500

    update command system for core 0.4

--- AstralPickups.cs
+++ AstralPickups.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using Astrum.AstralCore.Managers;
+using HarmonyLib;
 using MelonLoader;
 using System;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Reflection;
 using VRC.SDKBase;
 using VRC.Udon.Wrapper.Modules;
 
-[assembly: MelonInfo(typeof(Astrum.AstralPickups), "AstralPickups", "0.3.0", downloadLink: "github.com/Astrum-Project/AstralPickups")]
+[assembly: MelonInfo(typeof(Astrum.AstralPickups), "AstralPickups", "0.3.1", downloadLink: "github.com/Astrum-Project/AstralPickups")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
 [assembly: MelonOptionalDependencies("AstralCore")]
@@ -30,10 +31,7 @@ namespace Astrum
             HarmonyInstance.Patch(typeof(ExternVRCSDK3ComponentsVRCPickup).GetMethod(nameof(ExternVRCSDK3ComponentsVRCPickup.__set_proximity__SystemSingle)), hkNoOp);
 
             if (AppDomain.CurrentDomain.GetAssemblies().Any(f => f.GetName().Name == "AstralCore"))
-            {
-                if (UnityEngine.Application.version.Contains("1134"))
-                    External.RemoveWallCheck(External.FindWallCheck());
-            }
+                External.SetupCommands();
             else MelonLogger.Warning("AstralCore is missing, running at reduced functionality");
         }
 
@@ -48,23 +46,23 @@ namespace Astrum
 
         private static class External
         {
-            public static IntPtr FindWallCheck()
+            public static void SetupCommands()
             {
-                return AstralCore.Utils.PatternScanner.Scan(
-                    "GameAssembly.dll", 
-                    "0F 85 C9 00 00 00" + //    jne GameAssembly.dll + AE40E0
-                    "48 8B 0D ????????" + //    mov rcx, [GameAssembly.dll + 7200EC0]
-                    "F6 81 2F 01 00 00 02" + // test byte ptr [rcx+12F], 2
-                    "74 0E" + //                je GameAssembly.dll + AE4035
-                    "44 39 B1 E0 00 00 00" + // cmp [rcx + E0],r14d
-                    "75 05" + //                jne GameAssembly.dll + AE4035??
-                    "E8 ????????" + //          call GameAssembly.il2cpp_runtime_class_init
-                    "33 D2" //                  xor edx, edx
-                );
-            }
+                ModuleManager.Module module = new ModuleManager.Module("Pickups");
+                module.Register(new CommandManager.Command() { onExecute = new Func<string[], string>(_ =>
+                {
+                    foreach (VRC_Pickup pickup in UnityEngine.Object.FindObjectsOfType<VRC_Pickup>())
+                    {
+                        if (!pickup.IsHeld) continue;
+                        if (Networking.GetOwner(pickup.gameObject) != Networking.LocalPlayer)
+                            Networking.SetOwner(Networking.LocalPlayer, pickup.gameObject);
+                        pickup.Drop(); // may be unneeded
+                        // owner can possibly be restored
+                    }
 
-            public static void RemoveWallCheck(IntPtr address) => AstralCore.Utils.MemoryUtils.WriteBytes(address, new byte[6] { 0xE9, 0xCA, 0x00, 0x00, 0x00, 0x90 });
-            public static void RepairWallCheck(IntPtr address) => AstralCore.Utils.MemoryUtils.WriteBytes(address, new byte[6] { 0x0F, 0x85, 0xC9, 0x00, 0x00, 0x00 });
+                    return "";
+                }) }, "Drop");
+            }
         }
     }
 }
